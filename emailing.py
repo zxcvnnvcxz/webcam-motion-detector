6223ba5a896abd7c3c25eaf58a9219a444ac7ec7
EDWARD <edward.kang0301@gmail.com>
2024-10-25 05:44:06 +1100

    Initial Commit

--- emailing.py
+++ emailing.py
@@ -1,2 +1,28 @@
-def send_email():
-    print("Email was sent!")
+import os
+import imghdr
+import smtplib
+from email.message import EmailMessage
+
+PASSWORD = os.getenv("GMAIL_PW")
+SENDER = os.getenv("GMAIL_USER")
+RECEIVER = os.getenv("GMAIL_USER")
+
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New Customer has showed up."
+    email_message.set_content("To the reception please.")
+
+    with open(image_path, "rb") as file: # read binary
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+if __name__ == "__main__":
+    send_email(image_path="images/19.png")
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import glob
-
+import os
 import cv2
 import time
 from emailing import send_email
@@ -12,6 +12,13 @@ status_list = []
 count = 1
 save_time = 0.5
 last_detection_time = 0
+
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+
 
 while True:
     status = 0
@@ -27,7 +34,7 @@ while True:
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
     cv2.imshow("My  Video", dil_frame)
 
-    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours= cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
         if cv2.contourArea(contour) < 6000:
@@ -44,14 +51,16 @@ while True:
                 all_images = glob.glob("images/*.png")
                 index = int(len(all_images) / 2)
                 image_with_obj = all_images[index]
-
+            else:
+                image_with_obj = None
             last_detection_time = current_time
 
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        send_email(image_with_obj)
+        clean_folder()
 
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
